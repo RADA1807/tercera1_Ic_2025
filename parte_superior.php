d4083fe7bce4547c7b6e71d6060799af52699fea
RADA1807 <robert1807@hotmail.com>
2025-02-27 01:34:20 -0600

    Menu

---
+++ parte_superior.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="Spanish">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="Proyecto Tercera" content="Proyecto Tercera">
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+
+    <menu>
+    <div class="menu-up">
+        <a href="./">   
+            <img src="./Images/Rocket.png" alt=""height="50">
+        </a> 
+    </div>
+<div class="menu-down">
+<div class="menu-izquierda">
+    <a href="">News</a>
+    <a href="">Discord webhook</a>
+    <a href="">Database</a>
+    <a href="">Tools</a>
+    <a href="">Guides</a>
+    <a href="">Community</a>
+    <a href=""class="premiun">Premiun</a>
+    </div>
+
+    <div class="menu-derecha">
+    <a href="./">   
+        <img src="./Images/Mail.png" alt=""height="30">
+        <img src="./Images/Languages1.png" alt=""height="30">
+        <img src="./Images/h0o47b6r.png" alt=""height="30">
+        <img src="./Images/mxxaw5jj.png" alt=""height="30">
+        <img src="./Images/Whatsapp.jpg" alt=""height="30">
+        <img src="./Images/Facebook.png" alt=""height="30">
+    </a> 
+   </div>
+</div>
+</menu>
+
+
+
