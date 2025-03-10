4f6a5c6f7c16094d25afcc0c6ac21eca1364eb6f
RADA1807 <robert1807@hotmail.com>
2025-03-10 00:03:23 -0600

    Hamburger Icon

--- parte_inferior.php
+++ parte_inferior.php
@@ -12,3 +12,4 @@
 
 <script src="./script/accordion.js"></script>
 <script src="./script/cards.js"></script>
+<script src="./script/hamburger-icon.js"></script>
--- parte_superior.php
+++ parte_superior.php
@@ -7,34 +7,43 @@
     <link rel="stylesheet" href="./css/main.css">
 </head>
 
-    <menu>
+<menu>
     <div class="menu-up">
         <a href="./">   
             <img src="./Images/Rocket.png" alt=""height="55">
         </a> 
     </div>
-<div class="menu-down">
-<div class="menu-izquierda">
-    <a href="">News</a>
-    <a href="">Discord webhook</a>
-    <a href="">Database</a>
-    <a href="">Tools</a>
-    <a href="">Guides</a>
-    <a href="">Community</a>
-    <a href=""class="premiun">Premiun</a>
+    <div class="menu-down">
+        <div class="hamburger-icon">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        <div class="menu-container">
+            <div class="menu-izquierda">
+                <a href="">News</a>
+                <a href="">Discord webhook</a>
+                <a href="">Database</a>
+                <a href="">Tools</a>
+                <a href="">Guides</a>
+                <a href="">Community</a>
+                <a href=""class="premiun">Premiun</a>
+            </div>
+            
+        </div>
+        
+        <div class="menu-derecha">
+            <a href=""> Login</a>
+            <a href="./">
+                <img src="./Images/Mail.png" alt=""height="20">
+                <img src="./Images/processed_image.png" alt=""height="20">
+                <img src="./Images/Full screen.png" alt=""height="25">
+                <img src="./Images/Notificaciones.png" alt=""height="25">
+                <img src="./Images/Whatsapp.png" alt=""height="25">
+                <img src="./Images/Facebook.png" alt=""height="25">
+            </a>
+        </div>
     </div>
-
-    <div class="menu-derecha">
-    <a href="./">   
-        <img src="./Images/Mail.png" alt=""height="30">
-        <img src="./Images/processed_image.png" alt=""height="30">
-        <img src="./Images/Full screen.png" alt=""height="30">
-        <img src="./Images/Notificaciones.png" alt=""height="30">
-        <img src="./Images/Whatsapp.png" alt=""height="30">
-        <img src="./Images/Facebook.png" alt=""height="30">
-    </a> 
-   </div>
-</div>
 </menu>
 
 
